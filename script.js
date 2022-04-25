4cc4001798ba8956c339120864e474007c2ff5d6
CadenEras <gries.melissa@outlook.fr>
2022-04-25 15:35:02 +0200

    small changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const config = './config.json';
-let locationURL = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${config.apiKey}&q=${searchedLocation}`;
+let locationURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchedLocation}&limit=1&appid=${config.apiKey}&`;
 let locationRequest = new XMLHttpRequest();
 locationRequest.open('GET', locationRequest);
 locationRequest.responseType = 'json';
